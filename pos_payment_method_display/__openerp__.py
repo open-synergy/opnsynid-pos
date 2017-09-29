682d16122691991990252788afa32c4c540f6ddd
mike <viriyananda.michael@gmail.com>
2017-09-29 20:34:16 +0700

    Adding new module pos_payment_method_display

---
+++ pos_payment_method_display/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Point Of Sale - Payment Methods Display",
+    "version": "8.0.1.0.0",
+    "summary": "Payment Methods Display",
+    "category": "Point Of Sale",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "point_of_sale"
+    ],
+    "data": [
+        'views/assets_backend.xml',
+        "views/account_journal_view.xml"
+	],
+}
