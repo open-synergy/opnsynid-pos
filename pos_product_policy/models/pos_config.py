18e313dbbb4536d0ecea50046a7e2caef16ebe07
mike <viriyananda.michael@gmail.com>
2017-10-08 20:05:41 +0700

    Revision

--- pos_product_policy/models/pos_config.py
+++ pos_product_policy/models/pos_config.py
@@ -9,16 +9,16 @@ class PosConfig(models.Model):
     _inherit = "pos.config"
 
     @api.depends(
-        "allowed_product_categ_ids",
+        "allowed_pos_categ_ids",
         "allowed_product_ids",
         "allowed_product_ids.sale_ok")
     def _compute_all_allowed_product_ids(self):
         obj_product = self.env["product.product"]
         for config in self:
             products = config.allowed_product_ids
-            category_ids = config.allowed_product_categ_ids.ids
+            category_ids = config.allowed_pos_categ_ids.ids
             criteria = [
-                ("categ_id", "in", category_ids),
+                ("pos_categ_id", "in", category_ids),
                 ("sale_ok", "=", True),
                 ("available_in_pos", "=", True)
             ]
@@ -28,12 +28,12 @@ class PosConfig(models.Model):
     limit_product_selection = fields.Boolean(
         string="Limit Product Selection",
     )
-    allowed_product_categ_ids = fields.Many2many(
-        string="Allowed Product Categories",
-        comodel_name="product.category",
-        relation="rel_config_2_product_categ",
+    allowed_pos_categ_ids = fields.Many2many(
+        string="Allowed POS Product Categories",
+        comodel_name="pos.category",
+        relation="relation_config_2_pos_categ",
         column1="config_id",
-        column2="product_categ_id",
+        column2="pos_categ_id",
     )
     allowed_product_ids = fields.Many2many(
         string="Allowed Product",
@@ -42,7 +42,7 @@ class PosConfig(models.Model):
             ("sale_ok", "=", True),
             ("available_in_pos", "=", True)
         ],
-        relation="rel_config_2_product",
+        relation="relation_config_2_product",
         col1="config_id",
         col2="product_id",
     )
--- pos_product_policy/static/src/js/main-1.js
+++
@@ -1,4 +0,0 @@
-openerp.pos_product_policy = function(instance){
-    var module = instance.point_of_sale;
-    pos_product_policy_models(instance,module);
-}
