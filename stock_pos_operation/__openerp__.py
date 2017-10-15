89c560ba00d3f3c007deb983e904a13d155ab8be
Andhitia Rama <andhitia.r@gmail.com>
2017-10-15 10:50:56 +0700

    [8.0.1.0.0] stock_pos_operation

---
+++ stock_pos_operation/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "PoS Order Operation",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Stock Management",
+    "depends": [
+        "stock_warehouse_technical_information",
+        "stock_operation_type_location",
+        "point_of_sale",
+    ],
+    "data": [
+        "views/stock_warehouse_view.xml"
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
