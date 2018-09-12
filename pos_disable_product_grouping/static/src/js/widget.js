a94c42a5bd5e1ce2bb5d975d658e1cdcb1bc7212
Andhitia Rama <andhitia.r@gmail.com>
2018-09-12 16:53:02 +0700

    [8.0.1.0.1] pos_disable_product_grouping

--- pos_disable_product_grouping/static/src/js/widget.js
+++ pos_disable_product_grouping/static/src/js/widget.js
@@ -5,14 +5,15 @@ function pos_disable_product_grouping_widget(instance, module){
     module.OrderWidget.include({
         set_value: function(val){
             var order = this.pos.get("selectedOrder");
-            if (!order.getSelectedLine().get_product().pos_grouping_ok){
+            var mode = this.numpad_state.get('mode');
+            if (mode == 'quantity' && !order.getSelectedLine().get_product().pos_grouping_ok){
                 alert(_t("You cannot change qty of this product"));
             }
             else{
                 this._super(val);
             }
         }
-    });  
-    
-    
+    });
+
+
 };
