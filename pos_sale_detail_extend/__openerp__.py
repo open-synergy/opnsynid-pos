c45535119b6edfa112bb407efb7664ce47c03c80
mikevhe18 <viriyananda.michael@gmail.com>
2019-08-27 18:54:28 +0700

    [8.0.1.0.0][pos_sale_detail_extend]

---
+++ pos_sale_detail_extend/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Point Of Sale - Sale Details Extend",
+    "version": "8.0.1.0.0",
+    "category": "Point Of Sale",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "point_of_sale"
+    ],
+    "data": [
+    ],
+}
